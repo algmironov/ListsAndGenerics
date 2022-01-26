48c9a7a4aa6713e50dc678cd4604c90af74ab794
Alexey Mironov <al.g.mironov@yandex.ru>
2022-01-26 13:39:08 +0300

    Initial commit

---
+++ src/main/java/pro/skystudent/listsandgenerics/ListsAndGenericsApplication.java
@@ -0,0 +1,13 @@
+package pro.skystudent.listsandgenerics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListsAndGenericsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListsAndGenericsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/skystudent/listsandgenerics/ListsAndGenericsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.skystudent.listsandgenerics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsAndGenericsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
